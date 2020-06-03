6d4537883f4a56da0e655bac8a1749b99f70d52a
Enrique López Jiménez <loj.enrique@gmail.com>
2020-06-03 18:59:23 -0500

    Updated icon and splash screen

--- src/styles/components/shared/button.ts
+++ src/styles/components/shared/button.ts
@@ -1,11 +1,13 @@
 import { StyleSheet } from 'react-native';
 import { fonts, typography } from '../../base/typography';
 import { colors } from '../../base/colors';
+import { shadows } from '../../base/shadows';
 
 export const button = StyleSheet.create({
     touchable: {
         flex: 1,
         height: 'auto',
+        ...shadows.primary
     },
     gradient: {
         borderRadius: 4,
